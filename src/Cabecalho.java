588c7957212b20e557b49baae30ed4d81f2ff66b
Aline2408 <Kaiquemga15@gmail.com>
2024-09-24 20:47:10 -0300

    perguntas

--- src/Cabecalho.java
+++ src/Cabecalho.java
@@ -1,10 +1,11 @@
 public class Cabecalho {
-    public static void main(String[] args) {
+
+    public void escreva() {
         System.out.println("Faculdade: Unifan");
         System.out.println("Aluno(a): Aline da Silva Negreiros");
         System.out.println("Turma: ESN2 - A");
         System.out.println("Professor: Brenno Pimenta");
         System.out.println("Bem vindo ao quiz Matemático");
-
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,35 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Cabecalho c = new Cabecalho();
+        c.escreva();
+
+
+        Questao q1 = new Questao();
+        q1.pergunta = "1 - Qual o valor de 7+3x2?";
+        q1.opcaoA = "a - 10";
+        q1.opcaoB = "b - 13";
+        q1.opcaoC = "c - 17";
+        q1.opcaoD = "d - 20";
+        q1.opcaoE = "e - 23";
+        q1.correta = "b";
+
+        q1.escrevaQuestao();
+        String resposta = q1.leiaResposta();
+        q1.isCorreta(resposta);
+
+        Questao q2 = new Questao();
+        q2.pergunta = "1 - Qual é a raiz quadrada de 144?";
+        q2.opcaoA = "a - 10";
+        q2.opcaoB = "b - 18";
+        q2.opcaoC = "c - 17";
+        q2.opcaoD = "d - 11";
+        q2.opcaoE = "e - 12";
+        q2.correta = "e";
+
+        q2.escrevaQuestao();
+        resposta = q2.leiaResposta();
+        q2.isCorreta(resposta);
     }
 }
