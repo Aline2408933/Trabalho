c45e4acec9ca878df9154a168cb51840aef46f1e
Aline2408 <Kaiquemga15@gmail.com>
2024-09-24 19:54:39 -0300

    inicio

---
+++ src/Cabecalho.java
@@ -0,0 +1,10 @@
+public class Cabecalho {
+    public static void main(String[] args) {
+        System.out.println("Faculdade: Unifan");
+        System.out.println("Aluno(a): Aline da Silva Negreiros");
+        System.out.println("Turma: ESN2 - A");
+        System.out.println("Professor: Brenno Pimenta");
+        System.out.println("Bem vindo ao quiz Matemático");
+
+    }
+}
