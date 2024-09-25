5905a27620d1c099dc6fbc0a0968ee2211a52101
Aline2408 <Kaiquemga15@gmail.com>
2024-09-25 08:23:23 -0300

    pergguntas da 3-5

--- src/Main.java
+++ src/Main.java
@@ -21,7 +21,7 @@ public class Main {
         q1.isCorreta(resposta);
 
         Questao q2 = new Questao();
-        q2.pergunta = "1 - Qual é a raiz quadrada de 144?";
+        q2.pergunta = "2 - Qual é a raiz quadrada de 144?";
         q2.opcaoA = "a - 10";
         q2.opcaoB = "b - 18";
         q2.opcaoC = "c - 17";
@@ -32,5 +32,45 @@ public class Main {
         q2.escrevaQuestao();
         resposta = q2.leiaResposta();
         q2.isCorreta(resposta);
+
+        Questao q3 = new Questao();
+        q3.pergunta = "3 - Se um triângulo tem um ângulo de 90 graus, qual é o nome desse triângulo?";
+        q3.opcaoA = "a - Equilátero";
+        q3.opcaoB = "b - Isósceles";
+        q3.opcaoC = "c - Escaleno";
+        q3.opcaoD = "d - Retângulo";
+        q3.opcaoE = "e - Obtusângulo";
+        q3.correta = "d";
+
+        q3.escrevaQuestao();
+        resposta = q3.leiaResposta();
+        q3.isCorreta(resposta);
+
+        Questao q4 = new Questao();
+        q4.pergunta = "4 - Qual é a fração equivalente a 0,25?";
+        q4.opcaoA = "a - 1/2";
+        q4.opcaoB = "b - 1/4";
+        q4.opcaoC = "c - 1/6";
+        q4.opcaoD = "d - 3/4";
+        q4.opcaoE = "e - 2/5";
+        q4.correta = "b";
+
+        q4.escrevaQuestao();
+        resposta = q4.leiaResposta();
+        q4.isCorreta(resposta);
+
+        Questao q5 = new Questao();
+        q5.pergunta = "5 - Qual é a soma dos ângulos internos de um triângulo?";
+        q5.opcaoA = "a - 90º";
+        q5.opcaoB = "b - 120º";
+        q5.opcaoC = "c - 360º";
+        q5.opcaoD = "d - 180º";
+        q5.opcaoE = "e - 240";
+        q5.correta = "d";
+
+        q5.escrevaQuestao();
+        resposta = q5.leiaResposta();
+        q5.isCorreta(resposta);
+
     }
 }
